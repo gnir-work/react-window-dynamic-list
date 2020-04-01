6e092f6b168f78ffc19058df475da6fdeaaccd6c
Nir Geller <gnir.work@gmail.com>
2020-04-01 18:27:06 +0300

    Removed redundant import.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { VariableSizeList } from "react-window";
 import MeasurableList from "./MeasurableList";
 
