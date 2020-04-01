367bd51d5138555be00be32337d5df87c86c806e
Nir Geller <gnir.work@gmail.com>
2020-04-01 18:26:50 +0300

    Created better example page.

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { ExampleComponent } from 'react-window-dynamic-list'
+import DynamicList from 'react-window-dynamic-list'
 import 'react-window-dynamic-list/dist/index.css'
 import { generateCommands } from "./utils";
+import { Input } from "antd";
+
+import "antd/dist/antd.compact.min.css"
+import "./App.css"
+
+const commands = generateCommands();
 
 
 const App = () => {
-  return <ExampleComponent data={generateCommands()} />
+  const [filter, setFilter] = useState("");
+  const filteredCommands = commands.filter(command => JSON.stringify(command).includes(filter));
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  }
+
+
+  return <div>
+    <header>
+      <h2>Dynamic list Example</h2>
+    </header>
+    <content>
+      <div className="filter-container">
+        <Input value={filter} onChange={handleFilterChange} placeholder="Try filtering..." />
+      </div>
+      <div className="dynamic-list-container">
+        <DynamicList data={filteredCommands} width={600} height={600}>
+          {({ index, style }) => (
+            <div style={style}>
+              <pre style={{ whiteSpace: "inherit" }}>{filteredCommands[index].output}</pre>{" "}
+            </div>
+          )}
+        </DynamicList>
+      </div>
+    </content>
+  </div>
 }
 
 export default App
