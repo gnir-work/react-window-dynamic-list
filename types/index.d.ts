3ef23e85cd8a1c21e4a6b0ea99f45084d4239697
Nir Geller <gnir.work@gmail.com>
2021-07-10 04:02:43 -0700

    Fixed measurementContainerElement types - Receive a component instead of a invalid function.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,5 @@
-import { ReactNode, ReactChildren, PropsWithChildren, CSSProperties } from "react";
-import { VariableSizeListProps } from "react-window";
+import {ComponentType, CSSProperties, PropsWithChildren} from "react";
+import {VariableSizeListProps} from "react-window";
 
 /**
  * All of the data should contain an id property that will be used
@@ -23,7 +23,7 @@ export class CacheType {
   values: cacheValuesType[];
   constructor(initialValues: cacheValuesType);
   clearCache(): void;
-} 
+}
 
 export interface DynamicListProps<K extends BasicDataType> extends Omit<VariableSizeListProps, "itemSize" | "itemCount" | "estimatedItemSize"> {
   data: K[];
@@ -33,13 +33,13 @@ export interface DynamicListProps<K extends BasicDataType> extends Omit<Variable
     width: boolean;
     height: boolean;
   };
-  measurementContainerElement?: (style: CSSProperties, children: ReactChildren) => ReactNode;
+  measurementContainerElement?: ComponentType<{ style: CSSProperties }>;
   debug?: boolean;
 }
 
 /**
  * A virtualized list which handles item of varying sizes.
- * Read the props section in the README for additional information on the added props above the basic react-window ones. 
+ * Read the props section in the README for additional information on the added props above the basic react-window ones.
  * Read the implementation section in the README for additional information on the general algorithm.
  */
 export default function DynamicList<K extends BasicDataType>(props: PropsWithChildren<DynamicListProps<K>>): JSX.Element;
